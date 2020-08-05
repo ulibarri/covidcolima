ff00262328a353d67e2836c81ea2d4ebee4df2fd
ulibarri <ulibarriireta@gmail.com>
2020-08-05 02:23:38 -0500

    ago 4th upodate recuperados info fix

--- js/demo/chart-column-demo.js
+++ js/demo/chart-column-demo.js
@@ -1,15 +1,15 @@
-var valeur = [14, 9, 10, 9, 0, 0, 1, 1, 0, 1];
+var valeur = [16, 4, 5, 10, 1, 1, 0, 0, 0, 0];
 var date = [
-    "Manzanillo 31.11%",
-    "Tecomán 20.00%",
-    "V. De A. 22.22%",
-    "Colima 20.00%",
-    "Cuauhtémoc 0%",
-    "Armería 0%",
-    "Coquimatlán 2.22%",
-    "Minatitlán 2.22%",
+    "Manzanillo 43.24%",
+    "Tecomán 10.81%",
+    "V. De A. 13.51%",
+    "Colima 27.03%",
+    "Cuauhtémoc 2.70%",
+    "Armería 2.70%",
+    "Coquimatlán 0%",
+    "Minatitlán 0%",
     "Ixtlahuacán 0%",
-    "Comala 2.22%"
+    "Comala 0%"
 
 ];
 
--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["Manzanillo", "Tecomán", "V. Álvarez", "Colima"],
     datasets: [{
-      data: [774, 446, 277, 382],
+      data: [790, 450, 282, 392],
       backgroundColor: ['#e74a3b', '#1cc88a', '#36b9cc', '#4e73df'],
       hoverBackgroundColor: ['#fd7e14', '#17a673', '#2c9faf', '#2e59d9'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
@@ -47,7 +47,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["Hombres", "Mujeres"],
     datasets: [{
-      data: [147, 81],
+      data: [158, 96],
       backgroundColor: ['#13855c', '#6f42c1'],
       hoverBackgroundColor: ['#1cc88a', '#9b7cd4'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
