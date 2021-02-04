3ab4412db1a2c40d4b5469f3958d48afd43c40f2
ulibarri <ulibarriireta@gmail.com>
2021-02-03 22:19:15 -0600

    bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 // Load plugins
 const autoprefixer = require("gulp-autoprefixer");
-const browsersync = require("./node_modules/browser-sync").create();
+const browsersync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const del = require("del");
 const gulp = require("gulp");
